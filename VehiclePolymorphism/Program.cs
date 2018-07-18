2d9a291a92083998117daba307929cb26e7ff953
Monica Björk <m.bjork95@gmail.com>
2018-07-18 21:38:38 +0200

    I think I'm done and I think I understand.

--- VehiclePolymorphism/Program.cs
+++ VehiclePolymorphism/Program.cs
@@ -7,26 +7,25 @@ using System.Threading.Tasks;
 namespace VehiclePolymorphism {
     class Program {
         static void Main(string[] args) {
-            Truck truck = new Truck();
+            Truck truck = new Truck(150);
             Vehicle vehicle = (Vehicle) truck;
-            Console.WriteLine($"With a shipment of {truck.GetShipment}kg, the top speed is {vehicle.Calc_Topspeed()}km/h");
+            truck.Print();
             Console.ReadKey(true);
-            vehicle.Horn();
+            truck.Horn();
             Console.ReadKey(true);
         }
     }
 
     abstract class Vehicle {
         // Defining data
-        protected int horsepowers = 150;
+        protected int horsepowers = 0;
         protected string color = "red";
-        protected string name = "volvo";
 
-        public Vehicle() {
-            // Not sure what to do here
+        public Vehicle(int horsepower) {
+            this.horsepowers = horsepower;
         }
 
-        public abstract double Calc_Topspeed();
+        public abstract double Calc_Topspeed(int hp);
 
         public void Horn() {
             Console.WriteLine("honk hooonk");
@@ -37,13 +36,22 @@ namespace VehiclePolymorphism {
         // Defining data
         private int shipment = 1200;
 
+        public Truck(int horsepower) :base(horsepower) {
+            Calc_Topspeed(horsepowers);
+        }
+
         public int GetShipment {
             get { return shipment; }
         }
         
-        public override double Calc_Topspeed() {
-            double topSpeed = (horsepowers * 1.6) - (shipment/100);
+        public override double Calc_Topspeed(int hp) {
+            double topSpeed = (hp * 1.6) - (shipment/100);
             return topSpeed;
+        }
+
+        public void Print() {
+            Console.WriteLine($"A {color} truck was made!\n" +
+                              $"It has as much as {horsepowers}horsepower and with a shipment of {GetShipment}kg it has a top speed of {Calc_Topspeed(horsepowers)}km/h.");
         }
     }
 }
